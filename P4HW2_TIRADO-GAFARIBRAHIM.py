19275ea53aeb55724310beab1b6e908d0e0726ef
N64u <103143881+N64u@users.noreply.github.com>
2022-05-07 20:13:09 -0400

    Add files via upload

---
+++ P4HW2_TIRADO-GAFARIBRAHIM.py
@@ -0,0 +1,71 @@
+# CTI-110
+# P4HW2 - Pizza Order
+# Ibrahim Tirado-Gafar
+# 5.7.2022
+#
+
+import math
+
+def main():
+    num_students = float(input('How many students do you want to order pizza for?'))
+
+    pizza_needed = 0
+    pizza_cost = 5
+    before_tax = 0
+    tax = 0.06
+    total_price = 0
+
+    per_list = [1.5,2.0,3.0]
+    pizza_per = float(input("How many students do you want to order pizza for?(1.5, 2, 3)"))
+    run = True
+
+    while run == True:
+    
+        while pizza_per not in per_list:
+            print('\nINVALID ENTRY!!!!')
+            user_per = float(input('Enter number of people per pizza again.(1.5, 2 or 3):'))
+
+        if pizza_per == 1.5:
+            pizza_needed = num_students / pizza_per # example 27 / 1.5
+            before_tax = (pizza_needed * pizza_cost)# example 18 * 5
+            tax_applied = before_tax * tax          # example 90 * 0.06
+            total_price = before_tax + tax_applied  # example 90 + 5.4
+            pizza_needed = math.ceil(pizza_needed)  # Total price of pizza
+            print()
+            print('----Pizza Order-------')
+            print(f'Number of Students :{num_students}')
+            print(f'Pizzas Needed :{pizza_needed}') # Shows how many pizzas are needed
+            print(f'Price ${total_price:.2f}')         # Total price of pizza
+            print('----------------------')
+
+        elif pizza_per == 2:
+            pizza_needed = num_students / pizza_per # example 27 / 2
+            before_tax = (pizza_needed * pizza_cost)# example 13.5 * 5
+            tax_applied = before_tax * tax          # example 67.5 * 0.06
+            total_price = before_tax + tax_applied  # example 67.5 + 4.05
+            pizza_needed = math.ceil(pizza_needed)  # Total price of pizza
+            print()
+            print('----Pizza Order-------')
+            print(f'Number of Students :{num_students}')
+            print(f'Pizzas Needed :{pizza_needed}') # Shows how many pizzas are needed
+            print(f'Price ${total_price:.2f}')        # Total price of pizza
+            print('----------------------')
+
+        elif pizza_per == 3:
+            pizza_needed = num_students / pizza_per # example 27 / 3
+            before_tax = (pizza_needed * pizza_cost)# example 9 * 5
+            tax_applied = before_tax * tax          # example 45 * 0.06
+            total_price = before_tax + tax_applied  # example 45 + 2.7
+            pizza_needed = math.ceil(pizza_needed)
+            print()
+            print('----Pizza Order-------')
+            print(f'Number of Students :{num_students}')
+            print(f'Pizzas Needed :{pizza_needed}') # Shows how many pizzas are needed
+            print(f'Price ${total_price:.2f}')        # Total price of pizza
+            print('----------------------')
+        
+        run = input('\nWant to run program again?(Y / N)')
+    
+        if run == 'y':
+            main()
+main()
